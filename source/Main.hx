d60a1fc04c43087c46f6c69d8471ff15a3e47b6b
Nice-Pain <107368503+Nice-Pain@users.noreply.github.com>
2023-01-04 17:55:04 +0000

    Main

--- source/Main.hx
+++ source/Main.hx
@@ -70,7 +70,7 @@ class Main extends Sprite
 		SUtil.doTheCheck();
 
 		Paths.getModFolders();
-		addChild(new FlxGame(gameWidth, gameHeight, initialState, zoom, framerate, framerate, skipSplash, startFullscreen));
+		addChild(new FlxGame(gameWidth, gameHeight, initialState, #if (flixel < "5.0.0") zoom, #end framerate, framerate, skipSplash, startFullscreen));
 
 		fpsVar = new FPS(10, 3, 0xFFFFFF);
 		//addChild(fpsVar);
