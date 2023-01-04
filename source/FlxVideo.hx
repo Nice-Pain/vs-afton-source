618e7f2ca1d1c652ee0555dec70fbebd0c3d98ab
Nice-Pain <107368503+Nice-Pain@users.noreply.github.com>
2023-01-04 17:17:18 +0000

    FlxVideo maybe fixed

--- source/FlxVideo.hx
+++ source/FlxVideo.hx
@@ -4,8 +4,8 @@ import openfl.net.NetStream;
 import openfl.events.NetStatusEvent;
 import openfl.media.Video;
 #elseif android
-import extension.webview.WebView;
-import android.AndroidTools;
+import extension.videoview.VideoView;
+import android.net.Uri;
 #else
 import openfl.events.Event;
 import vlc.VlcBitmap;
@@ -49,13 +49,14 @@ class FlxVideo extends FlxBasic {
 		});
 		netStream.play(name);
 
-	#elseif android
-        WebView.playVideo(AndroidTools.getFileUrl(name), true);
-        WebView.onComplete = function(){
-		if (finishCallback != null){
-			finishCallback();
-		}
-        }
+	        #elseif android
+
+                VideoView.playVideo(Uri.fromFile(name));
+                VideoView.onCompletion = function(){
+		        if (finishCallback != null){
+			        finishCallback();
+		        }
+                }             
 
 		#elseif desktop
 		// by Polybius, check out PolyEngine! https://github.com/polybiusproxy/PolyEngine
